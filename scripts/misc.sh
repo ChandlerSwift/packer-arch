e58846dd72b7782e2834b7506354746a8c8bbdb0
Chandler Swift <chandler+pearson@chandlerswift.com>
2020-05-29 10:50:50 -0500

    Finish dotfiles setup

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-sudo password -d chandler
+# Set up Dotfiles
+cd ~
+git init
+git remote add origin https://github.com/ChandlerSwift/dotfiles.git
+git fetch
+git checkout -f master
+git submodule update --init --recursive  # Vundle
+
+# Install Vim packages
+vim -c "VundleInstall" -c "qa"
+
+# Remove password
+sudo passwd -d chandler
