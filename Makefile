01318fcacb27bdb2b8ed3f5d1f523dab1ab406c6
Chandler Swift <chandler@chandlerswift.com>
2020-11-10 13:51:02 -0600

    Fix copy_homedir

--- Makefile
+++ Makefile
@@ -8,5 +8,4 @@ build:
 
 .PHONY: sync_home
 sync_home:
-	#rsync --archive --delete --info=progress2 chandler@172.16.18.200: sync
 	packer build -var 'copy_homedir=true' archlinux.json
--- scripts/misc.sh
+++ scripts/misc.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 
-if [ "$COPY_HOMEDIR" -eq "true" ]; then
+# Set up SSH auth from packer content dir
+mkdir -p ~/.ssh
+mv /tmp/content/.ssh/* ~/.ssh/
+
+if [ "$COPY_HOMEDIR" = "true" ]; then
 	echo "Copying home dir"
-	rsync -a --delete --info=progress2 /tmp/sync /home/chandler
-else # Setting up fresh
-	# Set up SSH auth from packer content dir
-	mkdir -p ~/.ssh
-	mv /tmp/content/.ssh/* ~/.ssh/
-
-	# TODO: copy other files from /tmp/content
-	# * gnupg
-	# * ...
-
-
+	ssh-keyscan pearson-dev.edenprairie.chandlerswift.com >> ~/.ssh/known_hosts
+	rsync --archive --delete --info=progress2 pearson-dev.edenprairie.chandlerswift.com:/home/chandler/ /home/chandler/
+else
 	# Set up Dotfiles
 	cd ~
 	git init
@@ -20,13 +16,13 @@ else # Setting up fresh
 	git fetch
 	git checkout -f arch
 	git submodule update --init --recursive  # Vundle
+
+	# Install Vim packages
+	vim +PluginInstall +qall
 fi
 
 # Set zsh to default shell
 sudo chsh -s /usr/bin/zsh chandler
-
-# Install Vim packages
-vim +PluginInstall +qall
 
 # Remove password
 sudo passwd -d chandler
@@ -39,6 +35,3 @@ sudo systemctl start systemd-timesyncd.service
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 sudo usermod -aG docker chandler
-
-[ -d /tmp/content/.ssh ] && mv /tmp/content/.ssh /home/chandler/
-[ -d /tmp/content/.gnupg ] && mv /tmp/content/.gnupg /home/chandler/
