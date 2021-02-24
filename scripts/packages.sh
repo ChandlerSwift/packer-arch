f3205a0d56a598d4ef0d7f6351c06e232bce8e9d
Chandler Swift <chandler@chandlerswift.com>
2021-02-24 10:59:24 -0600

    Change package list to array
    This lets us add comments

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,49 +1,52 @@
-#!/bin/sh
+#!/bin/bash
 
 # potentially also include alsa-utils, iwd, qt5-wayland, pulseaudio, node
 # We already have `base base-devel linux dhcpcd grub openssh sudo` from base install
 # For non-virtualized installs, also include grub, alsa-utils, pulseaudio
-sudo pacman -Sy --noconfirm \
-	alacritty \
-	dhcpcd \
-	docker \
-	firefox \
-	freerdp \
-	git \
-	go \
-	grim \
-	i3status \
-	imagemagick \
-	ipython \
-	jq \
-	kubectl \
-	mako \
-	man-db \
-	man-pages \
-	openconnect \
-	parallel \
-	pv \
-	python \
-	python-pip \
-	remmina \
-	rsync \
-	slurp \
-	sshfs \
-	strace \
-	sway \
-	swayidle \
-	swaylock \
-	ttf-fira-code \
-	units \
-	unzip \
-	vim \
-	wl-clipboard \
-	wofi \
-	xorg-server-xwayland \
-	youtube-dl \
-	yq \
-	zsh \
+PACKAGES=(
+	alacritty
+	dhcpcd
+	docker
+	firefox
+	freerdp # RDP support in Remmina
+	git
+	go
+	grim
+	i3status
+	imagemagick
+	ipython
+	jq
+	kubectl
+	mako # notifications, comparable to dunst
+	man-db
+	man-pages
+	openconnect # VPN
+	parallel
+	pv
+	python
+	python-pip
+	remmina
+	rsync
+	slurp
+	sshfs
+	strace
+	sway
+	swayidle
+	swaylock
+	ttf-fira-code
+	units
+	unzip
+	vim
+	wl-clipboard
+	wofi # dmenu replacement
+	xorg-server-xwayland
+	youtube-dl
+	yq
+	zsh
 	zsh-completions
+)
+
+sudo pacman -Sy --noconfirm ${PACKAGES[@]}
 
 # install yay (https://github.com/Jguer/yay)
 cd /tmp
